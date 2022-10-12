ffddb84f33ad5e9ac2d3af65d63d112e0458abfb
Андрей <knopikanic@gmail.com>
2022-10-12 19:14:20 +0300

    DZ_14

--- src/test/java/cloud/autotests/tests/TestBase.java
+++ src/test/java/cloud/autotests/tests/TestBase.java
@@ -2,28 +2,34 @@ package cloud.autotests.tests;
 
 import cloud.autotests.config.Project;
 import cloud.autotests.helpers.AllureAttachments;
-import cloud.autotests.helpers.DriverSettings;
 import cloud.autotests.helpers.DriverUtils;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.junit5.AllureJunit5;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import static java.lang.String.format;
 
 
-@ExtendWith({AllureJunit5.class})
-public class TestBase {
+public class TestBase  {
+
     @BeforeAll
-    static void beforeAll() {
-        DriverSettings.configure();
-    }
-
-    @BeforeEach
-    public void beforeEach() {
+    static void configure() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("browserName", System.getProperty("browserName", "chrome"));
+        capabilities.setCapability("browserVersion", System.getProperty("browserVersion", "99"));
+        Configuration.browserCapabilities = capabilities;
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+
+        if (System.getProperty("remote") != null)
+            Configuration.remote = format("https://%s:%s@%s/wd/hub", System.getProperty("user"), System.getProperty("password"), System.getProperty("remote"));
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
     }
 
     @AfterEach
